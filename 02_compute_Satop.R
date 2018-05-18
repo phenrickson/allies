39a24be98ba803413c7b9feacecc98447ce372d2
Phil Henrickson <phil.henrickson@gmail.com>
2018-05-18 12:49:28 -0400

    Add files via upload

--- 02_compute_Satop.R
+++ 02_compute_Satop.R
@@ -1,4 +1,4 @@
-# Create alliance portfolios
+# Create S-ATOP Using ATOP portfolios
 library(tidyr)
 library(dplyr)
 library(foreign)
@@ -8,10 +8,10 @@ library(doParallel)
 
 #setwd("F:/Dropbox/FSU/FSU Summer 2017/Mark_Project/")
 setwd("/Users/phenrickson/Dropbox/FSU/Mark_Projects/")
-#setwd("C:/Users/PoliSci Lab_2/Downloads/Mark_Projects/Mark_Projects/")
 
 # load in ms data
 ms_dat<-as.data.table(read.dta("ms_data_v2.dta"))
+load("cleanDat.Rdata")
 
 # load in portfolio data
 load("portfolio_cowalliance.Rdata")
@@ -22,8 +22,27 @@ load("portfolio_milinst_atop.Rdata")
 source("S.R")
 source("cleanCCode.R")
 
-# let's try computing US and Britain for 1970
-foo<-portfolio_cowalliance$`1970` %>%
+
+### Let's look at the ATOP data
+foo<-portfolio_atopms2$`1816`
+
+# ATOP seems to have NAs take the place of 0s
+foo1<-portfolio_cowalliance$`1816`
+rm(foo, foo1)
+
+# the portfolios were constructed using the same countries as cowalliance
+# we should be safe to recode NAs as 0
+atopms2<-suppressWarnings(foreach(i=1:length(portfolio_atopms2)) %do% {
+        portfolio_atopms2[[i]] %>% 
+                as.data.frame() %>%
+                mutate_all(funs(replace(., is.na(.), 0)))
+})
+allyears<-sort(unique(cleanDat$year))
+names(atopms2)<-do.call(c, lapply(allyears, toString))
+
+
+# now look at US and UK in 1970
+foo<-atopms2$`1970` %>%
         filter(ccode=="002" | ccode== "200") %>%
         select(-ccode, -year)
 
@@ -32,22 +51,23 @@ b<-as.vector(as.matrix(foo[2,]))
 w<-rep(1 / length(a), length(a))
 m<-3
 
-s_new<-S(a, b, w, m)
-s_new
+s_atop<-S(a, b, w, m)
+s_atop
+
 # works
 
 # what was the S score for this pairing in the original?
 ms_dat %>%
         filter(ccode1==002 & ccode2==200 & year==1970) %>%
         select(s_un_glo)
-# alright we're pretty close
+# okay pretty similar to S
 
 ### loop over US Britain for all years
 allyears<-sort(unique(ms_dat$year))
 
-test<-foreach(i=1:length(portfolio_cowalliance), .combine=rbind.data.frame) %do% {
+test<-foreach(i=1:length(atopms2), .combine=rbind.data.frame) %do% {
         
-        foo<-portfolio_cowalliance[[i]] %>%
+        foo<-atopms2[[i]] %>%
                 filter(ccode=="002" | ccode== "200") %>%
                 select(-ccode, -year)
         
@@ -56,10 +76,10 @@ test<-foreach(i=1:length(portfolio_cowalliance), .combine=rbind.data.frame) %do%
         w<-rep(1 / length(a), length(a))
         m<-3
         
-        s_new<-S(a, b, w, m)
+        s_atop<-S(a, b, w, m)
         
         out<-data.frame(year=allyears[i],
-                        S_new=s_new)
+                        S_atop=s_atop)
         out
 }
 
@@ -89,16 +109,16 @@ g1<-ggplot(meltdf, aes(x=year, y=value, color=variable))+
         scale_colour_brewer(palette = "Paired")+
         theme_bw()
 g1
-# looks good
+# pretty similar to S
 
 
 ### US Russia
 c1<-"002"
 c2<-"365"
 
-test<-foreach(i=1:length(portfolio_cowalliance), .combine=rbind.data.frame) %do% {
+test<-foreach(i=1:length(atopms2), .combine=rbind.data.frame) %do% {
         
-        foo<-portfolio_cowalliance[[i]] %>%
+        foo<-atopms2[[i]] %>%
                 filter(ccode==c1 | ccode== c2) %>%
                 select(-ccode, -year)
         
@@ -107,10 +127,10 @@ test<-foreach(i=1:length(portfolio_cowalliance), .combine=rbind.data.frame) %do%
         w<-rep(1 / length(a), length(a))
         m<-3
         
-        s_new<-S(a, b, w, m)
+        s_atop<-S(a, b, w, m)
         
         out<-data.frame(year=allyears[i],
-                        S_new=s_new)
+                        S_atop=s_atop)
         out
 }
 
@@ -146,9 +166,9 @@ g2
 c1<-"200"
 c2<-"255"
 
-test<-foreach(i=1:length(portfolio_cowalliance), .combine=rbind.data.frame) %do% {
+test<-foreach(i=1:length(atopms2), .combine=rbind.data.frame) %do% {
         
-        foo<-portfolio_cowalliance[[i]] %>%
+        foo<-atopms2[[i]] %>%
                 filter(ccode==c1 | ccode== c2) %>%
                 select(-ccode, -year)
         
@@ -157,10 +177,10 @@ test<-foreach(i=1:length(portfolio_cowalliance), .combine=rbind.data.frame) %do%
         w<-rep(1 / length(a), length(a))
         m<-3
         
-        s_new<-S(a, b, w, m)
+        s_atop<-S(a, b, w, m)
         
         out<-data.frame(year=allyears[i],
-                        S_new=s_new)
+                        S_atop=s_atop)
         out
 }
 
@@ -195,9 +215,9 @@ g3
 c1<-"740"
 c2<-"710"
 
-test<-foreach(i=1:length(portfolio_cowalliance), .combine=rbind.data.frame) %do% {
+test<-foreach(i=1:length(atopms2), .combine=rbind.data.frame) %do% {
         
-        foo<-portfolio_cowalliance[[i]] %>%
+        foo<-atopms2[[i]] %>%
                 filter(ccode==c1 | ccode== c2) %>%
                 select(-ccode, -year)
         
@@ -206,10 +226,10 @@ test<-foreach(i=1:length(portfolio_cowalliance), .combine=rbind.data.frame) %do%
         w<-rep(1 / length(a), length(a))
         m<-3
         
-        s_new<-S(a, b, w, m)
+        s_atop<-S(a, b, w, m)
         
         out<-data.frame(year=allyears[i],
-                        S_new=s_new)
+                        S_atop=s_atop)
         out
 }
 
@@ -242,85 +262,86 @@ library(gridExtra)
 grid.arrange(g1, g2, g3, g4, ncol=2)
 
 
-### Compute S codes for all countries
+### Compute S_atop codes for all countries
 ccodes<-cleanCCode(sort(unique(c(ms_dat$ccode1, ms_dat$ccode2))))
 
 # loop over years
-S_new<-foreach(i=1:length(portfolio_cowalliance), .combine=rbind.data.frame) %do% {
+S_atop<-foreach(i=1:length(portfolio_atopms2), .combine=rbind.data.frame) %do% {
         
-                # grab one state
-                c1<-ccodes[i]
-                others<-ccodes[-which(ccodes<=c1)]
+        # grab one state
+        c1<-ccodes[i]
+        others<-ccodes[-which(ccodes<=c1)]
+        
+        # then pair with all others
+        hold_ccodes<-foreach(j=1:length(others), .combine=rbind.data.frame) %do% {
                 
-                # then pair with all others
-                hold_ccodes<-foreach(j=1:length(others), .combine=rbind.data.frame) %do% {
+                c2<-others[j]
+                
+                # then run across all years
+                hold_years<-foreach(k=1:length(allyears), .combine=rbind.data.frame) %do% {
                         
-                        c2<-others[j]
+                        foo<-portfolio_atopms2[[k]] %>%
+                                filter(ccode==c1 | ccode== c2) %>%
+                                select(-ccode, -year)
                         
-                        # then run across all years
-                        hold_years<-foreach(k=1:length(allyears), .combine=rbind.data.frame) %do% {
-                                
-                                foo<-portfolio_cowalliance[[k]] %>%
-                                        filter(ccode==c1 | ccode== c2) %>%
-                                        select(-ccode, -year)
-                                
-                                a<-as.vector(as.matrix(foo[1,]))
-                                b<-as.vector(as.matrix(foo[2,]))
-                                w<-rep(1 / length(a), length(a))
-                                m<-3
-                                
-                                s_new<-S(a, b, w, m)
-                                
-                                out<-data.frame(ccode1=c1,
-                                                ccode2=c2,
-                                                year=allyears[k],
-                                                S_new=s_new)
-                                out
-                                
-                        }
-                        print(c(c1, c2))
-                        hold_years
+                        a<-as.vector(as.matrix(foo[1,]))
+                        b<-as.vector(as.matrix(foo[2,]))
+                        w<-rep(1 / length(a), length(a))
+                        m<-3
+                        
+                        s_atop<-S(a, b, w, m)
+                        
+                        out<-data.frame(ccode1=c1,
+                                        ccode2=c2,
+                                        year=allyears[k],
+                                        S_atop=s_atop)
+                        out
                         
                 }
-                print(c1)
-                hold_ccodes
+                print(c(c1, c2))
+                hold_years
+                
+        }
+        print(c1)
+        hold_ccodes
 }
 
+
+
 # save
-save(S_new, file="S_new.Rdata")
-load("S_new.Rdata")
+save(S_atop, file="S_atop.Rdata")
+load("S_atop.Rdata")
 
 # load
 load("cleanDat.Rdata")
 
 # compare
-check<-as.tbl(left_join(cleanDat, S_new, by=c("ccode1", "ccode2", "year")))
+check<-as.tbl(left_join(cleanDat, S_atop, by=c("ccode1", "ccode2", "year")))
 
 # correlation
-corr<-cor(check$s_un_glo, check$S_new, use="pairwise")
+corr<-cor(check$s_un_glo, check$S_atop, use="pairwise")
 
 # plot
 g<- check %>%
-  dplyr::select(ccode1, ccode2, year, s_un_glo, S_new)
+        dplyr::select(ccode1, ccode2, year, s_un_glo, S_atop)
 
-p<-ggplot(g, aes(x=S_new, y=s_un_glo))+
-  geom_point(alpha=0.2)+
-  geom_abline(slope=1)+
-  ylab("S_Old")+
-  xlab("S_New")+
-  annotate("text", x=0.25, y=0.75, label= paste("corr=", paste(round(corr,3)), sep="")) + 
-  theme_bw()
+p<-ggplot(g, aes(x=S_atop, y=s_un_glo))+
+        geom_point(alpha=0.2)+
+        geom_abline(slope=1)+
+        ylab("S_Old")+
+        xlab("S_atop")+
+        annotate("text", x=0.25, y=0.75, label= paste("corr=", paste(round(corr,3)), sep="")) + 
+        theme_bw()
 p
 
 # oh god don't do this
 #library(plotly)
 
 #p2<-plot_ly(
- # g, x=~S_new, y=~s_un_glo,
+# g, x=~S_atop, y=~s_un_glo,
 #  text=~paste(ccode1, ccode2, year)
 #)
 
 #p2
 
 ### stop
-
